e868566e3fa6e16a0e9ef15fac8ed58a3a5b773f
Gordon <3225297@gmail.com>
2017-03-19 02:15:02 +0800

    add coundownform test

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,7 +1,5 @@
 var React = require('react');
 
-
-
 var Clock = React.createClass({
   getDefaultProps: function() {
     totalSeconds : 0
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,9 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Navigation/>
-      <div>
-        <p>Main.jsx Rendered</p>
+      <div className="row">
+      <div className="column small-centered medium-6 large-4">
+
         {props.children}
+      </div>
       </div>
     </div>
   );
